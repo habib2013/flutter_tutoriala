b570f1e198b62583a8499c16856d54422ced5cef
habib2013 <nairabet25@gmail.com>
2020-07-06 14:26:30 +0100

    nother messagei

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyApState();
+  }
+}
+
+class MyApState extends State<MyApp>{
+  var questionIndex = 0;
+
+  void answerQuestions(){
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
 @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+  var questions = ['what\'s your name 22','what\'s your favorite food',];
+
+  // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(title: Text('My First App'),
+        ),
+        body: Column(
+          children:[
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text('Answer 1'),onPressed: answerQuestions,),
+            RaisedButton(child: Text('Answer 2'),onPressed: null,),
+            RaisedButton(child: Text('Answer 3'),onPressed: null,),
+          ],
+        ),
 
       )
     );
